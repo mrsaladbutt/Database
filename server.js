ba2181bf00bc4a81dfb9a98cfbe5fa58dec6fd43
Hudozhnyk Olexii <150086385+mrsaladbutt@users.noreply.github.com>
2024-05-04 09:17:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-
-const checkAuth = (req, res, next) => {
-   
-    const authHeader = req.headers['authorization'];
-    if (authHeader && authHeader.startsWith('Bearer ')) {
-       
-        req.isAuthenticated = true;
-    } else {
-        
-        req.isAuthenticated = false;
-    }
-
-    next();
-};
-
 server.use(middlewares);
-
-server.use(checkAuth);
 server.use(router);
 
-server.listen(port, () => {
-  console.log(JSON Server is running on port ${port});
-});
+server.listen(port);
